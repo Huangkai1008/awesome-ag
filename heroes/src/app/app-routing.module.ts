dcb6ad774a6a600e24b74d70d938d3a151197eac
huangkai <18778335525@163.com>
2019-01-02 17:19:37 +0800

    路由链接代替onSelected

--- heroes/src/app/app-routing.module.ts
+++ heroes/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HeroesComponent} from "./heroes/heroes.component";
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroesComponent}
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent}, // 添加英雄详情视图
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, // 添加默认路由
 ];
 
 @NgModule({
--- heroes/src/app/heroes/heroes.component.ts
+++ heroes/src/app/heroes/heroes.component.ts
@@ -10,7 +10,6 @@ import {HeroService} from "../hero.service";
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   // 这个参数同时做了两件事：1. 声明了一个私有 heroService 属性，2. 把它标记为一个 HeroService 的注入点。
   // 当 Angular 创建 HeroesComponent 时，依赖注入系统就会把这个 heroService 参数设置为 HeroService 的单例对象。
@@ -21,9 +20,6 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
